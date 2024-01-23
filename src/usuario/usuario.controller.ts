96c9c3064bc2ebe90c718e074b133f5290a33ccf
Ismael Oliveira <ismael6991@live.com>
2024-01-23 08:48:56 -0300

    Criando a entidade Usuário

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,18 +1,27 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CriaUsuarioDTO } from './dto/CriaUsuario.dto';
 import { UsuarioRepository } from './usuario.repository';
+import { UsuarioEntity } from './usuario.entity';
+import { v4 as uuid} from 'uuid'
 
 @Controller('/usuarios')
 export class UsuarioController {
   constructor(private usuarioRepository: UsuarioRepository) {}
 
   @Post()
-  async criaUsuario(@Body() dadosDoUsuario) {
-    this.usuarioRepository.salvar(dadosDoUsuario);
-    return dadosDoUsuario;
+  async criaUsuario(@Body() dadosDoUsuario: CriaUsuarioDTO) {
+    const usuarioEntity = new UsuarioEntity()
+    usuarioEntity.email = dadosDoUsuario.email
+    usuarioEntity.senha = dadosDoUsuario.senha
+    usuarioEntity.nome = dadosDoUsuario.nome
+    usuarioEntity.id = uuid()
+
+    this.usuarioRepository.salvar(usuarioEntity);
+    return {id: usuarioEntity.id, message: 'Usuário criado com sucesso!'}
   }
 
   @Get()
-  async listaUsuarios() {
+  async listUsuarios() {
     return this.usuarioRepository.listar();
   }
 }
